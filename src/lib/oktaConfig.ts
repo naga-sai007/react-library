947e7ffccc7e3c1f4f68769eaeb53e991bcee3ef
naga-sai007 <60782699+naga-sai007@users.noreply.github.com>
2023-08-13 19:48:14 +0530

    Update oktaConfig.ts

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oaamgt6wqgCWi4sI5d7',
     issuer: 'https://dev-67919541.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'http://react-library-bucket.s3-website.ap-south-1.amazonaws.com/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
