58a005d1f468531827e51fe6aeee1d96395e2a7a
naga-sai007 <60782699+naga-sai007@users.noreply.github.com>
2023-08-13 20:36:43 +0530

    Update oktaConfig.ts

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
     clientId: '0oaamgt6wqgCWi4sI5d7',
-    issuer: 'https://dev-67919541.okta.com/oauth2/default',
+    issuer: 'http://dev-67919541.okta.com/oauth2/default',
     redirectUri: 'http://react-library-bucket.s3-website.ap-south-1.amazonaws.com/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
